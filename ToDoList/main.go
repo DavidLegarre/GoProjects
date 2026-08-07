f4283a42712b1c7c41c5334b19da663e24356910
davidlegarre <davidls920@gmail.com>
2026-08-07 15:09:10 +0200

    Add initial Go module and implement basic ToDo list structure

---
+++ ToDoList/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	todo := todoEntry{name: "Buy groceries"}
+	fmt.Println(todo)
+}
---
+++ ToDoList/todoObject.go
@@ -0,0 +1,5 @@
+package main
+
+type todoEntry struct {
+	name string
+}
