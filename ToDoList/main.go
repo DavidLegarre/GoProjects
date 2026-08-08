86f329ce9e1ed91e4487bf784387862e84654650
davidlegarre <davidls920@gmail.com>
2026-08-08 13:33:59 +0200

    Remove unused TodoEntry initialization from main function

--- ToDoList/main.go
+++ ToDoList/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"todolist/types"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	todo := types.TodoEntry{Name: "Buy groceries"}
-	fmt.Println(todo)
 	Run()
 }
