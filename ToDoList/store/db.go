325ef8a19a5bcc0c3abd0b5e907807b98f6ead5d
davidlegarre <davidls920@gmail.com>
2026-08-10 00:56:33 +0200

    Refactor ReadDB function to remove unnecessary error handling and logging for improved clarity

--- ToDoList/store/db.go
+++ ToDoList/store/db.go
@@ -2,25 +2,19 @@ package store
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"os"
 	"todolist/types"
 )
 
 func ReadDB() (*ItemStore, error) {
 	data, err := readJsonFile()
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) == false {
-			fmt.Printf("Unexpected error occured %s", err)
-		}
 		return nil, err
 	}
 
 	var todoList types.TodoList
 	err = json.Unmarshal(data, &todoList)
 	if err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
 		return nil, err
 	}
 
