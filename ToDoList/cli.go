129ffe692f7a8c7bfbaa213930ad03da8a7e797c
davidlegarre <davidls920@gmail.com>
2026-08-10 13:26:47 +0200

    Refactor command functions to improve error handling and return types; update loop and fetch functions for better error management

--- ToDoList/cli.go
+++ ToDoList/cli.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"todolist/store"
 )
@@ -13,7 +14,7 @@ type CommandArguments struct {
 	itemStore *store.ItemStore
 }
 
-var commands = map[string]func(*CommandArguments){
+var commands = map[string]func(args *CommandArguments) error{
 	"a": cmdAdd,
 	"r": cmdRemove,
 	"l": cmdList,
@@ -45,12 +46,15 @@ func loadStore() (*store.ItemStore, error) {
 	return itemStore, nil
 }
 
-func loop(itemStore *store.ItemStore) {
+func loop(itemStore *store.ItemStore) error {
 	sc := bufio.NewScanner(os.Stdin)
 	for {
-		input := fetch("Enter command (a=add, r=remove, l=list, e=exit): ", sc)
+		input, err := fetch("Enter command (a=add, r=remove, l=list, e=exit): ", sc)
+		if err != nil {
+			return err
+		}
 		if input == "e" {
-			return
+			return nil
 		}
 
 		command, ok := commands[input]
@@ -66,38 +70,50 @@ func loop(itemStore *store.ItemStore) {
 	}
 }
 
-func fetch(prompt string, sc *bufio.Scanner) string {
+func fetch(prompt string, sc *bufio.Scanner) (string, error) {
 	fmt.Print(prompt)
-	sc.Scan()
-	return sc.Text()
+	ok := sc.Scan()
+	if ok {
+		return sc.Text(), nil
+	}
+	return "", io.EOF
 }
 
-func cmdAdd(args *CommandArguments) {
-	entry := store.CreateEntry(fetch("Enter the name of the new entry: ", args.scanner))
+func cmdAdd(args *CommandArguments) error {
+	name, err := fetch("Enter the name of the new entry: ", args.scanner)
+	entry := store.CreateEntry(name)
 	args.itemStore.AddEntry(entry)
-	err := args.itemStore.Save()
+	err = args.itemStore.Save()
 	if err != nil {
 		fmt.Println("Error storing changes to the DB", err)
 	}
+	return err
 }
 
-func cmdRemove(args *CommandArguments) {
+func cmdRemove(args *CommandArguments) error {
 	args.itemStore.PrintEntries()
-	name := fetch("Enter the name of the entry to remove: ", args.scanner)
+	name, err := fetch("Enter the name of the entry to remove: ", args.scanner)
 
-	err := args.itemStore.RemoveEntryByName(name)
 	if err != nil {
 		fmt.Printf("Error removing %q: %v\n", name, err)
-		return
+		return err
+	}
+
+	err = args.itemStore.RemoveEntryByName(name)
+	if err != nil {
+		fmt.Printf("Error removing %q: %v\n", name, err)
+		return err
 	}
 	fmt.Printf("Entry '%s' removed successfully.\n", name)
 	err = args.itemStore.Save()
 	if err != nil {
 		fmt.Println("Error storing changes to the DB", err)
+		return err
 	}
-
+	return nil
 }
 
-func cmdList(args *CommandArguments) {
+func cmdList(args *CommandArguments) error {
 	args.itemStore.PrintEntries()
+	return nil
 }
